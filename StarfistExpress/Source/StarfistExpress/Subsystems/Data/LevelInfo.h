75aa9b39ab7c699e054d40c33472d881395eb153
DevKolIn <donghyeon.study@gmail.com>
2025-04-03 16:59:58 +0900

    [feat] 난이도 정보 저장하는 데이터 테이블 생성

---
+++ StarfistExpress/Source/StarfistExpress/Subsystems/Data/LevelInfo.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+	
+#include "Engine/DataTable.h"
+#include "LevelInfo.generated.h"
+
+class UTexture2D;
+
+USTRUCT(BlueprintType)
+struct FLevelInfo : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Level;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName LevelName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName AIName;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTexture2D* AITexture;
+};
